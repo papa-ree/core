bc96d06d9e341e4f44ab67e24b63815119dc805d
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2026-01-04 21:02:05 +0700

    Update input.blade.php

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -12,7 +12,7 @@
     <div class="relative" x-data="{ hidePassword: true }" wire:ignore>
         <input :type="hidePassword ? 'password' : 'text'" {{ $disabled ? 'disabled' : '' }} {{ $autofocus ? 'autofocus' : '' }} name="password" id="password" autocomplete="off" {!! $attributes->merge([
             'class' =>
-                'py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-emerald-300 focus:ring-emerald-300 dark:bg-slate-900 dark:border-gray-700 text-gray-800 dark:text-neutral-200',
+                'py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-purple-300 focus:ring-purple-300 dark:bg-slate-900 dark:border-gray-700 text-gray-800 dark:text-neutral-200',
         ]) !!}>
 
         <div class="absolute transform -translate-y-1/2 cursor-pointer top-1/2 right-4"
@@ -28,7 +28,7 @@
                 URL</label>
             <div class="relative">
                 <input type="text" id="bale-inline-add-on" name="bale-inline-add-on" {!! $attributes->merge([]) !!} {{ $autofocus ? 'autofocus' : '' }}
-                    class="py-3 px-4 ps-16 block w-full text-gray-900 placeholder-gray-500 transition-all duration-200 bg-white border border-gray-300 form-input dark:border-gray-600 rounded-xl dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent"
+                    class="py-3 px-4 ps-16 block w-full text-gray-900 placeholder-gray-500 transition-all duration-200 bg-white border border-gray-300 form-input dark:border-gray-600 rounded-xl dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
                     placeholder="www.example.com">
                 <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
                     <span class="text-sm text-gray-500 dark:text-neutral-500">{{ $attributes['addon'] }}</span>
@@ -76,7 +76,7 @@
     <input {{ $disabled ? 'disabled' : '' }} {{ $autofocus ? 'autofocus' : '' }} {!! $attributes->merge([
             'type' => 'text',
             'class' =>
-                'block w-full py-3 px-4 text-gray-900 placeholder-gray-500 transition-all duration-200 bg-white border border-gray-300 form-input dark:border-gray-600 rounded-xl dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent',
+                'block w-full py-3 px-4 text-gray-900 placeholder-gray-500 transition-all duration-200 bg-white border border-gray-300 form-input dark:border-gray-600 rounded-xl dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent',
         ]) !!}>
 @endif
 
@@ -103,14 +103,14 @@
                             <div class="rounded-lg cursor-pointer sm:flex hs-tooltip-toggle group"
                                 @click="$clipboard(generatedPassword); tooltipText = 'Copied'; setTimeout(() => { tooltipText = 'Copy'; showCopyIcon = true; }, 2000); showCopyIcon = !showCopyIcon">
                                 <input type="text" x-model="generatedPassword" disabled
-                                    class="py-2.5 sm:py-3 px-4 pe-11 block w-full border-gray-200 group-hover:bg-gray-100 transition duration-300 -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg sm:text-sm relative focus:z-10 focus:border-emerald-500 focus:ring-emerald-500 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">
+                                    class="py-2.5 sm:py-3 px-4 pe-11 block w-full border-gray-200 group-hover:bg-gray-100 transition duration-300 -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg sm:text-sm relative focus:z-10 focus:border-purple-500 focus:ring-purple-500 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">
                                 <span
                                     class="py-2.5 sm:py-3 px-4 inline-flex items-center min-w-fit w-full border border-gray-200 bg-gray-50 sm:text-sm text-gray-500 -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:w-auto sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
                                     <i data-lucide="clipboard"
                                         class="h-5 text-gray-700 transition-all duration-300 group-hover:rotate-12"
                                         :class="{ 'hidden': !showCopyIcon }"></i>
                                     <i data-lucide="check"
-                                        class="hidden h-5 transition-all duration-300 text-emerald-500 group-hover:rotate-12"
+                                        class="hidden h-5 transition-all duration-300 text-purple-500 group-hover:rotate-12"
                                         :class="{ 'hidden': showCopyIcon }"></i>
                                 </span>
                                 <span
@@ -142,7 +142,7 @@
                         <input label="Password Length" type="number" min="1" max="18" step="1" x-model="charsLength"
                             @input="generatePassword()" />
                         <input type="range"
-                            class="w-full bg-transparent cursor-pointer appearance-none disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden focus:border-emerald-500 focus:ring-emerald-500
+                            class="w-full bg-transparent cursor-pointer appearance-none disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden focus:border-purple-500 focus:ring-purple-500
                                                                                                                                                                 [&::-webkit-slider-thumb]:w-2.5
                                                                                                                                                                 [&::-webkit-slider-thumb]:h-2.5
                                                                                                                                                                 [&::-webkit-slider-thumb]:-mt-0.5
@@ -160,7 +160,7 @@
                                                                                                                                                                 [&::-moz-range-thumb]:appearance-none
                                                                                                                                                                 [&::-moz-range-thumb]:bg-white
                                                                                                                                                                 [&::-moz-range-thumb]:border-4
-                                                                                                                                                                [&::-moz-range-thumb]:border-emerald-400
+                                                                                                                                                                [&::-moz-range-thumb]:border-purple-400
                                                                                                                                                                 [&::-moz-range-thumb]:rounded-full
                                                                                                                                                                 [&::-moz-range-thumb]:transition-all
                                                                                                                                                                 [&::-moz-range-thumb]:duration-150
@@ -183,39 +183,39 @@
                     {{-- Character Type Options --}}
                     <div class="grid gap-2 mt-4 select-none sm:grid-cols-2">
                         <label for="charsLower"
-                            class="flex w-full p-3 text-sm bg-white border border-gray-200 rounded-lg focus:border-emerald-500 focus:ring-emerald-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                            class="flex w-full p-3 text-sm bg-white border border-gray-200 rounded-lg focus:border-purple-500 focus:ring-purple-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
                             <input type="checkbox" id="charsLower" x-ref="charsLower" x-model="charsLower"
                                 @input="generatePassword()"
-                                class="w-5 h-5 transition duration-200 text-emerald-400 form-checkbox rounded-xl focus:border-emerald-500 focus:ring-emerald-500"
+                                class="w-5 h-5 transition duration-200 text-purple-400 form-checkbox rounded-xl focus:border-purple-500 focus:ring-purple-500"
                                 id="charsLower" checked>
                             <span
                                 class="text-sm text-gray-500 ms-3 dark:text-neutral-400">{{ __('abcdefghijklmnopqrstuvwxyz') }}</span>
                         </label>
 
                         <label for="charsUpper"
-                            class="flex w-full p-3 text-sm bg-white border border-gray-200 rounded-lg focus:border-emerald-400 focus:ring-emerald-400 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                            class="flex w-full p-3 text-sm bg-white border border-gray-200 rounded-lg focus:border-purple-400 focus:ring-purple-400 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
                             <input type="checkbox" id="charsUpper" x-ref="charsUpper" x-model="charsUpper"
                                 @input="generatePassword()"
-                                class="w-5 h-5 transition duration-200 text-emerald-400 form-checkbox rounded-xl focus:border-emerald-500 focus:ring-emerald-500"
+                                class="w-5 h-5 transition duration-200 text-purple-400 form-checkbox rounded-xl focus:border-purple-500 focus:ring-purple-500"
                                 id="charsUpper" checked>
                             <span
                                 class="text-sm text-gray-500 ms-3 dark:text-neutral-400">{{ __('ABCDEFGHIJKLMNOPQRSTUVWXYZ') }}</span>
                         </label>
 
                         <label for="charsNumeric"
-                            class="flex w-full p-3 text-sm bg-white border border-gray-200 rounded-lg focus:border-emerald-500 focus:ring-emerald-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                            class="flex w-full p-3 text-sm bg-white border border-gray-200 rounded-lg focus:border-purple-500 focus:ring-purple-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
                             <input type="checkbox" id="charsNumeric" x-ref="charsNumeric" x-model="charsNumeric"
                                 @input="generatePassword()"
-                                class="w-5 h-5 transition duration-200 text-emerald-400 form-checkbox rounded-xl focus:border-emerald-500 focus:ring-emerald-500"
+                                class="w-5 h-5 transition duration-200 text-purple-400 form-checkbox rounded-xl focus:border-purple-500 focus:ring-purple-500"
                                 id="charsNumeric" checked>
                             <span class="text-sm text-gray-500 ms-3 dark:text-neutral-400">{{ __('123456789') }}</span>
                         </label>
 
                         <label for="charsSymbols"
-                            class="flex w-full p-3 text-sm bg-white border border-gray-200 rounded-lg focus:border-emerald-500 focus:ring-emerald-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                            class="flex w-full p-3 text-sm bg-white border border-gray-200 rounded-lg focus:border-purple-500 focus:ring-purple-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
                             <input type="checkbox" id="charsSymbols" x-ref="charsSymbols" x-model="charsSymbols"
                                 @input="generatePassword()"
-                                class="w-5 h-5 transition duration-200 text-emerald-400 form-checkbox rounded-xl focus:border-emerald-500 focus:ring-emerald-500"
+                                class="w-5 h-5 transition duration-200 text-purple-400 form-checkbox rounded-xl focus:border-purple-500 focus:ring-purple-500"
                                 id="charsSymbols" checked>
                             <span
                                 class="text-sm text-gray-500 ms-3 dark:text-neutral-400">{{ __('~!@#$%^&*_+-=:;?,.') }}</span>
